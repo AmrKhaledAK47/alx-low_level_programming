da737625c0333d7d85a5c7ff9fc4c2dd0430de32
Amr Khaled Othman <2002amrkhaled@gmail.com>
2023-06-20 03:15:17 -0700

    0-putchar.c added

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include"main.h"
+/**
+ * main - Entry point
+ *
+ * Description: 'print a statement'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char strr[] = "_putchar";
+int i;
+for (i = 0; i < 8; i++)
+{
+_putchar(strr[i]);
+}
+_putchar('\n');
+return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+int _putchar(char c);
+void print_alphabet(void);
+#endif
