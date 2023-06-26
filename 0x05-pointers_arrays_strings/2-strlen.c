db23ddbada1581c0fed850e4ded3264dfeb5ed42
Amr Khaled Othman <2002amrkhaled@gmail.com>
2023-06-26 10:18:04 -0700

    2-strlen.c added

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include"main.h"
+/**
+ * _strlen - length of string.
+ * @s: variable
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _strlen(char *s)
+{
+int x = strlen(s);
+return (x);
+}
