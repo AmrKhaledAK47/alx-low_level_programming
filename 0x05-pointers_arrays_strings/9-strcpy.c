3916c0acd753718155736574834231adb05ebb9e
Amr Khaled Othman <2002amrkhaled@gmail.com>
2023-06-26 19:43:14 -0700

    9-strcpy.c added

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include"main.h"
+/**
+ * *_strcpy - prints array.
+ * @dest: array
+ * @src: size of array
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+char *_strcpy(char *dest, char *src)
+{
+int i = -1;
+do {
+i++;
+dest[i] = src[i];
+} while (src[i] != '\0');
+return (dest);
+}
